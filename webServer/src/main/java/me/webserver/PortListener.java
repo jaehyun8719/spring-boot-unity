f59f0ca297dda5ae1d92d68c53a7f3479c6dcf85
jaehyun <jaehyun8719@gmail.com>
2019-02-24 23:40:26 +0900

    Embedded Web Servers

---
+++ webServer/src/main/java/me/webserver/PortListener.java
@@ -0,0 +1,29 @@
+package me.webserver;
+
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author : kjh
+ * github: https://github.com/jaehyun8719
+ * email: jaehyun8719@gmail.com
+ * <p>
+ * Date: 2019-02-24
+ * Copyright(©) 2019 by jaehyun.
+ */
+@Component
+public class PortListener implements ApplicationListener<ServletWebServerInitializedEvent> {
+
+
+    @Override
+    public void onApplicationEvent(ServletWebServerInitializedEvent servletWebServerInitializedEvent) {
+
+        ServletWebServerApplicationContext applicationContext = servletWebServerInitializedEvent.getApplicationContext();
+
+        System.out.println(applicationContext.getWebServer().getPort());
+    }
+}
---
+++ webServer/src/main/java/me/webserver/WebserverApplication.java
@@ -0,0 +1,13 @@
+package me.webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebserverApplication.class, args);
+    }
+
+}
