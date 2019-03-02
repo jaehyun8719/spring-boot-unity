ac16f1aa947d989549a9848182188f2c465af919
진실 <jaehyun8719@gmail.com>
2019-03-02 19:04:08 +0900

    프로파일 추가 1. 프로파일 활성화 2. 프로파일 추가 3. 프로파일용 프로퍼티

---
+++ externalizedConfiguration/src/main/java/me/externalconfig/config/BaseConfiguration.java
@@ -0,0 +1,26 @@
+package me.externalconfig.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author : 진실
+ * github: https://github.com/jaehyun8719
+ * email: jaehyun8719@gmail.com
+ * <p>
+ * Date: 2019-03-02
+ * Description:
+ * Copyright(©) 2019 by jaehyun.
+ **/
+@Profile("prod")
+@Configuration
+public class BaseConfiguration {
+
+    @Bean
+    public String hello() {
+        return "hello prod";
+    }
+}
